e26267faf3476d1dd27705b4b13fbf4f8d5852ec
DavidMikeSimon <david.mike.simon@gmail.com>
2010-05-16 04:16:58 +0000

    Okay, successfully generating something like what the up mirror file will be

--- lib/app/models/offline_mirror/mirror_file.rb
+++ lib/app/models/offline_mirror/mirror_file.rb
@@ -1,24 +1,25 @@
+require 'zlib'
+
 module OfflineMirror
 	#private
 	
 	class MirrorFile
+		# TODO Move this to the worker
 		def self.create_up_mirror_for(group_id)
-			f = MirrorFile.new
+			f = MirrorFile.new(group_id)
 			f.set_standard_title("Uploadable")
-			f.message = "<p>This is a " + OfflineMirror::app_name " data file for upload.</p>"
-			f.message += "<p>Please use the website at <a href=\"" + OfflineMirror::online_url + "\">" + OnlineMirror::online_url + "</a> to upload it.</p>"
-			f.save_to_tmp
+			f.message = "<p>This is a " + OfflineMirror::app_name + " data file for upload.</p>"
+			f.message += "<p>Please use the website at <a href=\"" + OfflineMirror::online_url + "\">" + OfflineMirror::online_url + "</a> to upload it.</p>"
+			return f
 		end
 		
+		# TODO Move this to the worker
 		def self.create_down_mirror_for(group_id)
-		end
-		
-		def self.load_from(ioh)
 		end
 		
 		def save_to_tmp
 			# FIXME Come up with an actual random file name
-			File.open(File.join(RAILS_ROOT, "tmp", "test")) do |ioh|
+			File.open(File.join(RAILS_ROOT, "tmp", "test"), "w") do |ioh|
 				write_to(ioh)
 			end
 		end
@@ -26,67 +27,58 @@ module OfflineMirror
 		def apply_to_database
 		end
 		
-		private
-		
-		def initialize(ioh = nil)
-			if ioh
-				read(ioh)
-			else
-				@message = ""
-				@title = ""
-				
-				@file_info = {
-					:created_by => OfflineMirror::app_online? ? "Online App" : ("Offline App for Group " + OfflineMirror::offline_group_id),
-					:created_at => Time.now,
-					:online_site => OfflineMirror::online_url,
-					:app => OfflineMirror::app_name,
-					:app_version => OfflineMirror::app_version,
-					:launcher_version => OfflineMirror::app_offline? ? OfflineMirror::launcher_version : "Online",
-					:operating_system => RUBY_PLATFORM
-				}
-				
-				@cargo_table = {:file_info => @file_info}
-			end
+		def initialize(group_id = nil)
+			@message = ""
+			@title = ""
+			
+			@file_info = {
+				:created_by => OfflineMirror::app_online? ? "Online App" : ("Offline App for Group " + OfflineMirror::offline_group_id),
+				:created_at => Time.now,
+				:online_site => OfflineMirror::online_url,
+				:app => OfflineMirror::app_name,
+				:app_version => OfflineMirror::app_version,
+				:launcher_version => OfflineMirror::app_offline? ? OfflineMirror::launcher_version : "Online",
+				:operating_system => RUBY_PLATFORM,
+				:for_group => group_id ? group_id : "Unknown"
+			}
+			
+			@cargo_table = {:file_info => @file_info}
 		end
 		
-		attr_accessible :message, :title, :cargo_table
+		attr_accessor :message, :title, :cargo_table
 		
 		def set_standard_title(mode)
 			@title = OfflineMirror::app_name + " " + mode + " Data"
 		end
 		
 		def clean_for_html_comment(s)
-			s.replace("--", "__").replace("<","[").replace(">","]")
+			s.to_s.gsub("--", "__").gsub("<", "[").gsub(">", "]")
 		end
 		
 		def write_to(ioh)
-			ioh.p "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
-			ioh.p "<!-- This file contains Offline Mirror data."
-			@file_info.keys.map({ |k| [k.to_s, @file_info[k]] }).sort.each do |k, v|
-				ioh.p clean_for_html_comment(k.titleize) + ": " + clean_for_html_comment(k)
+			ioh.puts "<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">\n"
+			ioh.puts "<!-- This file contains Offline Mirror data."
+			@file_info.keys.map{ |k| [k.to_s, @file_info[k]] }.sort.each do |k, v|
+				ioh.puts clean_for_html_comment(k.titleize) + ": " + clean_for_html_comment(v)
 			end
-			ioh.p "-->"
+			ioh.puts "-->"
 			
-			ioh.p "<html>"
-			ioh.p "<head><title>" + @title + "</title></head>"
-			ioh.p "<body>"
-			ioh.p @message
-			ioh.p "</body>"
-			ioh.p "</html>"
+			ioh.puts "<html>"
+			ioh.puts "<head><title>" + @title + "</title></head>"
+			ioh.puts "<body>"
+			ioh.puts @message
+			ioh.puts "</body>"
+			ioh.puts "</html>"
 			
 			@cargo_table.each_pair do |k, v|
-				name = Base64.b64encode(k)
-				data = Base64.b64encode(Zlib::Deflate::deflate(v))
-				verif = data.hash 
+				name = k.to_s
+				data = Base64.b64encode(Zlib::Deflate::deflate(v.to_json))
 				
-				ioh.p "<!-- CARGO SEGMENT"
-				ioh.p "NAME:"
-				ioh.p name
-				ioh.p "VERIF:"
-				ioh.p verif
-				ioh.p "DATA:"
-				ioh.p data
-				ioh.p "-->"
+				ioh.puts "<!-- CARGO SEGMENT"
+				ioh.puts name + ":"
+				ioh.puts data.hash.to_s
+				ioh.puts data
+				ioh.puts "-->"
 			end
 		end
 		
