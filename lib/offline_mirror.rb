11e2065ba38e65bdd699e9fb1b6516edf2ec7790
DavidMikeSimon <david.mike.simon@gmail.com>
2010-05-23 21:23:23 +0000

    Extending the todo list based on stuff I wrote down

--- lib/offline_mirror.rb
+++ lib/offline_mirror.rb
@@ -30,15 +30,17 @@ OfflineMirror::init
 # - When applying upmirror files, use all supplied permission checks and also check to make sure object being changed belongs to logged-in user's group
 # - The launcher should keep a log file
 # - Include recent log lines (for both Rails and the launcher) in generated up-mirror files, for debugging purposes
-# - For group data: Support use of several versioning columns, defined by model. Default is whatever's available of [:lock_version, :updated_at]
-	# - Include record whenever versioning column data is *different*, not just if it's higher
-	# - When app is offline, the updated_at columns is never used (offline clock might not be reliable)
-	# - If no versioning columns present, issue a single warning and then always treat records as dirty
-# - For global data: Always and only use updated_at if available, compare with date of last *confirmed* *received* down mirror for that group
-	# - If any records updated_at the same time as the down mirror's date, mark them for forced re-retrieval next down mirror (to prevent async losses)
-	# - If no versioning columns present, issue a single warning and then always treat records as dirty
-# - Properly deal with a new app version changing version column definition for one or more models (check against version_columns field)
 # - Use rails logger to note activity
 # - Document that the id of the group model itself is _never_ transformed, but all group_owned models and global models do have their id's transformed
 # - Scenario to deal with: offline app applies an initial down mirror, then makes local changes, then attempts to reapply _another_ initial down mirror
 	# - To deal with this, just ignore all non-global records in the second initial down mirror, since they should be identical
+# - Versioning the app : how to do this?
+# - When we import first down mirror w/ group data, set current_mirror_version to up_mirror_version + 1. OfflineMirror::system_state tries to do this; test.
+# - Make sure the filters catch typical ActiveRecord class methods: create, update, delete, etc.
+# - Split the offline environment into 'offline-production' and 'offline-test'
+# - Only update current_mirror_version after a confirmed transfer; that way, if 1st mirror file generated is lost, no big deal
+# - Don't accept down-mirror or up-mirror files that are the same version as the one already in place; version number must be greater
+	# - This causes problems if we aren't getting a nice back-and-forth of up-mirror and down-mirror, which is intentional
+# - In online app, increment current_mirror_version whenever anyone confirms that they received a down mirror
+# - In offline app, increment current_mirror_version whenever an up-mirror is confirmed
+# - In documentation, note that migrations which change records' primary keys will break synchronization
