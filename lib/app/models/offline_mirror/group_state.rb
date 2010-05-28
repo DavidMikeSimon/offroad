d6b7af658bede6bbb75fe6306451b4e048b2926d
DavidMikeSimon <david.mike.simon@gmail.com>
2010-05-28 02:50:21 +0000

    Fixed some problems in GroupState caused by the recent refactoring

--- lib/app/models/offline_mirror/group_state.rb
+++ lib/app/models/offline_mirror/group_state.rb
@@ -16,18 +16,19 @@ module OfflineMirror
 		def self.find_or_create_by_group(obj, opts = {})
 			ensure_group_base_model(obj)
 			rec = find_or_initialize_by_app_group_id(obj.id, opts)
-			if app_offline?
+			if OfflineMirror::app_offline?
 				# FIXME : Fill in last_installation_at, launcher_version, app_version
 				rec.last_known_offline_os = RUBY_PLATFORM
 				rec.offline = true
 			end
 			rec.save!
+			return rec
 		end
 		
 		private
 		
 		def self.ensure_group_base_model(obj)
-			raise "Passed object is not of a group_base_model" unless obj.class !== Class and obj.offline_mirror_mode == :group_base
+			raise "Passed object is not of a group_base_model" unless !(obj.class === Class) and obj.offline_mirror_mode == :group_base
 		end
 	end
 end
