06ffdd305f20d665e0a9e341d307a993a62a0784
DavidMikeSimon <david.mike.simon@gmail.com>
2010-05-29 18:04:04 +0000

    Tests are successfully detecting whether they are in online test or offline test mode

--- test/functional/groups_controller_test.rb
+++ test/functional/groups_controller_test.rb
@@ -1,7 +1,18 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
 class GroupsControllerTest < ActionController::TestCase
-	def test_truth
+	online_test :generate_down_mirror_file do
+		puts "ONLINE GDMF"
+		assert true
+	end
+
+	offline_test :generate_up_mirror_file do
+		puts "OFFLINE GUMF"
+		assert true
+	end
+
+	common_test :generate_mirror_file do
+		puts "COMMON GMF"
 		assert true
 	end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,3 +19,17 @@ ActiveSupport::TestCase.class_eval do
   
   fixtures :all
 end
+
+# Convenience methods to create tests that apply to online only, offline only, or both
+
+def online_test(name, &block)
+	common_test(name, &block) unless RAILS_ENV.start_with?("offline")
+end
+
+def offline_test(name, &block)
+	common_test(name, &block) if RAILS_ENV.start_with?("offline")
+end
+
+def common_test(name, &block)
+	define_method ("test_" + name.to_s).to_sym, &block
+end
