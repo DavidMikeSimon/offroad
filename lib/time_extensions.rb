d6d9e6689d6856b938c33ebb93de236e374cfc8d
DavidMikeSimon <david.mike.simon@gmail.com>
2010-06-12 00:32:14 +0000

    Also extending xmlschema of ActiveSupport::TimeWithZone

--- lib/time_extensions.rb
+++ lib/time_extensions.rb
@@ -1,6 +1,12 @@
-class Time
-  def xmlschema_with_default_subseconds
-    xmlschema_without_default_subseconds(12)
+module OfflineMirror
+  module TimeExtensions
+    def xmlschema_with_default_subseconds(precision = 12)
+      xmlschema_without_default_subseconds(precision)
+    end
   end
-  alias_method_chain :xmlschema, :default_subseconds
+end
+
+[Time, ActiveSupport::TimeWithZone].each do |cls|
+  cls.send(:include, OfflineMirror::TimeExtensions)
+  cls.alias_method_chain :xmlschema, :default_subseconds
 end
--- test/unit/time_xml_extension_test.rb
+++ test/unit/time_xml_extension_test.rb
@@ -1,16 +1,26 @@
 require File.dirname(__FILE__) + '/../test_helper'
 
-# This is a unit test on the plugin's monkey patch to increase default precision on Time#xmlschema
+# This is a unit test on the plugin's monkey patch to increase default precision on Time classes' xmlschema methods
 
 class TimeXMLExtensionTest < ActiveSupport::TestCase
+  def for_each_time_class
+    [Time, ActiveSupport::TimeZone.new("UTC")].each do |cls|
+      yield cls
+    end
+  end
+  
   online_test "time serialized to xml includes milliseconds" do
-    assert Time.now.xmlschema =~ /\d+:\d+\.\d+/
+    for_each_time_class do |cls|
+      assert cls.now.xmlschema =~ /\d+:\d+\.\d+/, "serialization ms check for #{cls.name}"
+    end
   end
   
   online_test "a time is unchanged by going thru xml and back" do
-    orig_time = Time::parse("1:23:45.678").gmtime
-    new_time = Time::xmlschema(orig_time.xmlschema)
-    assert_equal orig_time, new_time
+    for_each_time_class do |cls|
+      orig_time = cls::parse("1:23:45.678")
+      new_time = Time::xmlschema(orig_time.xmlschema)
+      assert_equal orig_time, new_time, "encoding change check for #{cls.name}"
+    end
   end
 end
 
