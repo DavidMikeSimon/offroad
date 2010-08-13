b1d495ad0ce87c821f0f715b6d6ab600b750c8e8
David Mike Simon <david.mike.simon@gmail.com>
2010-08-13 12:24:04 -0500

    Added some notes about how to address incoming records with bad foreign ids

--- lib/mirror_data.rb
+++ lib/mirror_data.rb
@@ -219,9 +219,12 @@ module OfflineMirror
                 foreign_rrs_source = foreign_rrs_source.for_group(@group) if foreign_model.offline_mirror_group_data?
                 foreign_rrs = foreign_rrs_source.find_by_remote_record_id(remote_foreign_id)
                 if !foreign_rrs
-                  # If the foreign record doesn't already exist, then it hasn't yet been imported
-                  # Just create an empty one for now to be filled in later, so we have a known local id to point at
-                  # Later when it's imported, we will update the same record based on this RRS
+                  # If the foreign record doesn't already exist, then it hasn't yet been imported.
+                  # Just create an empty one for now to be filled in later, so we have a known local id to point at.
+                  # Later when it's imported, we will update the same record based on this RRS.
+                  # FIXME: If we never end up importing the record, the placeholder should be deleted.
+                  # However, need to leave the RRS in case it is imported in another mirror file later.
+                  # Alternate idea: simply create then immediately destroy record, just to get an autoincremented id
                   foreign_rec_placeholder = foreign_model.new
                   foreign_rec_placeholder.bypass_offline_mirror_readonly_checks
                   foreign_rec_placeholder.save_without_validation
