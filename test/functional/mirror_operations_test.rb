c9c78e1288c1ae4c4f8615a4e4778449bf25b730
David Mike Simon <david.mike.simon@gmail.com>
2010-08-13 12:24:25 -0500

    Expanded the mirror operations functional test, revealing a bug which I will fix momentarily

--- test/functional/mirror_operations_test.rb
+++ test/functional/mirror_operations_test.rb
@@ -6,6 +6,7 @@ class MirrorOperationsTest < ActionController::TestCase
   cross_test "can create an offline app then use it to work with the online app" do
     mirror_data = ""
     in_online_app do
+      Group.create(:name => "Some Other Group")
       test_group = Group.create(:name => "Test Group")
       
       GlobalRecord.create(:title => "Important Announcement", :some_boolean => true)
@@ -24,10 +25,20 @@ class MirrorOperationsTest < ActionController::TestCase
       assert_equal 0, Group.count
       assert_equal 0, GlobalRecord.count
       assert_equal 0, GroupOwnedRecord.count
+      
       post :upload_initial_down_mirror, "mirror_data" => mirror_data
+      
       assert_equal 1, Group.count
-      assert_equal 2, GlobalRecord.count
+      assert_not_nil Group.find_by_name("Test Group")
+      
       assert_equal 3, GroupOwnedRecord.count
+      assert_not_nil GroupOwnedRecord.find_by_description("First Item")
+      assert_not_nil GroupOwnedRecord.find_by_description("Second Item")
+      assert_not_nil GroupOwnedRecord.find_by_description("Third Item")
+      
+      assert_equal 2, GlobalRecord.count
+      assert_not_nil GlobalRecord.find_by_title("Important Announcement")
+      assert_not_nil GlobalRecord.find_by_title("Trivial Announcement")
       
       group = Group.first
       group.name = "Renamed Group"
@@ -45,6 +56,12 @@ class MirrorOperationsTest < ActionController::TestCase
     end
     
     in_online_app do
+      GlobalRecord.find_by_title("Trivial Announcement").destroy
+      GlobalRecord.create(:title => "Yet Another Announcement")
+      rec = GlobalRecord.find_by_title("Important Announcement")
+      rec.title = "Very Important Announcement"
+      rec.save
+      
       post :upload_up_mirror, "id" => Group.find_by_name("Test Group").id, "mirror_data" => mirror_data
       
       assert_nil Group.find_by_name("Test Group")
@@ -54,6 +71,19 @@ class MirrorOperationsTest < ActionController::TestCase
       assert_not_nil GroupOwnedRecord.find_by_description("Absolutely The First Item")
       assert_nil GroupOwnedRecord.find_by_description("Second Item")
       assert_not_nil GroupOwnedRecord.find_by_description("Third Item")
+      
+      get :download_down_mirror, "id" => Group.find_by_name("Renamed Group").id
+      mirror_data = @response.binary_content
+    end
+    
+    in_offline_app do
+      post :upload_down_mirror, "id" => Group.first.id, "mirror_data" => mirror_data
+      
+      assert_equal 2, GlobalRecord.count
+      assert_nil GlobalRecord.find_by_title("Important Announcement")
+      assert_not_nil GlobalRecord.find_by_title("Very Important Announcement")
+      assert_nil GlobalRecord.find_by_title("Trivial Announcement")
+      assert_not_nil GlobalRecord.find_by_title("Yet Another Announcement")
     end
   end
 end
