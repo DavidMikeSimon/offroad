fdf87cdec4f4e5f972b3a76cb07c748ee252fb81
David Mike Simon <david.mike.simon@gmail.com>
2011-03-27 19:22:14 -0500

    Added a test to expose bug where Hobo permission methods not overriden properly

---
+++ test/unit/hobo_permissions_test.rb
@@ -0,0 +1,38 @@
+require File.expand_path(File.dirname(__FILE__) + '/../test_helper')
+
+# This is a unit test on the ability of model_extensions to override regular Hobo model permissions
+
+class HoboPermissionsTest < Test::Unit::TestCase
+  if HOBO_TEST_MODE
+    def setup
+      @guest = Guest.new
+      super
+    end
+
+    class HoboPermissionsTestModel < ActiveRecord::Base
+      hobo_model
+      set_table_name "broken_records"
+
+      def create_permitted?
+        true
+      end
+
+      def update_permitted?
+        true
+      end
+
+      def destroy_permitted?
+        true
+      end
+
+      acts_as_offroadable :global
+    end
+
+    offline_test "can override hobo permissions on group base data" do
+      rec = HoboPermissionsTestModel.new
+      force_save_and_reload(rec)
+      assert !rec.destroyable_by?(@guest)
+      assert !rec.updatable_by?(@guest)
+    end
+  end
+end
