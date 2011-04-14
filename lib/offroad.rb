f4e6b5e42fe883d34eb021a9195a413747642c09
David Mike Simon <david.mike.simon@gmail.com>
2011-04-14 14:16:22 -0500

    Monkey patching ar-extensions nextval bug

--- lib/offroad.rb
+++ lib/offroad.rb
@@ -11,6 +11,17 @@ $LOAD_PATH << path
 ActiveSupport::Dependencies.autoload_paths << path
 
 require 'ar-extensions' # External dependency
+# Monkey patch a bug in ar-extensions which breaks postgres compatibility
+module ActiveRecord # :nodoc:
+  module ConnectionAdapters # :nodoc:
+    class AbstractAdapter # :nodoc:
+      def next_value_for_sequence(sequence_name)
+        %{nextval('#{sequence_name}')}
+      end
+    end
+  end
+end
+
 
 require 'controller_extensions'
 class ActionController::Base
