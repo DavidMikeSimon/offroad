0dee8935c6600428204494ba3c24e531277d57b0
David Mike Simon <david.mike.simon@gmail.com>
2011-08-18 16:27:47 -0500

    Cutting down on logging nonsense

--- test/app_root/config/environments/test.rb
+++ test/app_root/config/environments/test.rb
@@ -15,3 +15,6 @@ config.action_controller.allow_forgery_protection    = false
 # The :test delivery method accumulates sent emails in the
 # ActionMailer::Base.deliveries array.
 config.action_mailer.delivery_method = :test
+
+# Avoid excessively huge log files
+config.log_level = :error
